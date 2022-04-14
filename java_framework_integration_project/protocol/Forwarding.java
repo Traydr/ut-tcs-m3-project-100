ad90fa35704440ccbcb293f83c13624201bf9fda
Mihaix2 <mihaisavulescu26@gmail.com>
2022-04-14 15:32:46 +0200

    some stuff added to Forwarding, IDK

--- java_framework_integration_project/protocol/Forwarding.java
+++ java_framework_integration_project/protocol/Forwarding.java
@@ -4,22 +4,30 @@ public class Forwarding {
 
     final int NODE_COUNT = 4;
     int myAddress;
+    int[] sources = new int[4];
+    int c = 0;
+    int counter;
     int[][] dataTable = new int[NODE_COUNT][NODE_COUNT];
 
     public Forwarding(int node) {
         myAddress = node;
+        sources[c] = node;
+        c++;
     }
 
-    public void init(int[][] forwardingTable) {
-        for (int i = 0; i < NODE_COUNT; i++) {
-            System.arraycopy(forwardingTable[i], 0, dataTable[i], 0, NODE_COUNT);
+    public void init(int neighbour, int step) {
+        dataTable[c][neighbour] = 1;
+        if (step > counter){
+            // TODO update datatable with new data
+        } else {
+
         }
     }
 
     public void pathFinding(int[][] forwardingTable) {
-        for (int k = 0; k < NODE_COUNT; ++k) {
-            for (int i = 0; i < NODE_COUNT; ++i) {
-                for (int j = 0; j < NODE_COUNT; ++j) {
+        for (int k = 0; k < NODE_COUNT; k++) {
+            for (int i = 0; i < NODE_COUNT; i++) {
+                for (int j = 0; j < NODE_COUNT; j++) {
                     if (i != j && forwardingTable[i][j] == 0) {
                         forwardingTable[i][j] = forwardingTable[i][k] * forwardingTable[k][j];
                     }
@@ -29,7 +37,7 @@ public class Forwarding {
     }
 
     public byte[] matrixToArray() {
-        byte[] pkt = new byte[16];
+        byte[] pkt = new byte[NODE_COUNT * NODE_COUNT];
         int count = 0;
         for (int i = 0; i < NODE_COUNT; i++) {
             for (int j = 0; j < NODE_COUNT; j++) {
@@ -38,6 +46,18 @@ public class Forwarding {
             }
         }
         return pkt;
+    }
+
+    public int[][] arrayToMatrix(Packet pck) {
+        byte[] data = pck.getData();
+        int[][] dataMatrix = new int[NODE_COUNT][NODE_COUNT];
+        for (int i = 0; i < NODE_COUNT; i++) {
+            for (int j = 0; j < NODE_COUNT; j++) {
+                dataMatrix[i][j] = data[4 * i + j];
+            }
+        }
+
+        return dataMatrix;
     }
 
 
