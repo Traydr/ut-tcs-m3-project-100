9040f5d2542c80724b4cb1bca29fe5f165c15b15
Traydr <Traydr@protonmail.com>
2022-04-21 00:35:18 +0200

    Something else

--- java_framework_integration_project/protocol/TimeOut.java
+++ java_framework_integration_project/protocol/TimeOut.java
@@ -1,31 +1,41 @@
 package protocol;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Random;
 
 public class TimeOut implements Runnable{
     int timeout;
     int random;
+    int timeoutType;
     LocalTime then;
     LocalTime now;
+    MyProtocol myProtocol;
 
-    public TimeOut(int time, int rand) {
+    public TimeOut(int time, int rand, MyProtocol protocol, int type) {
         this.timeout = time;
         this.random = rand;
+        this.myProtocol = protocol;
+        this.timeoutType = type;
+        this.then = LocalTime.now();
+        this.then = LocalTime.now();
     }
 
     public void startTimeout() {
-        then = LocalTime.now().plusSeconds(timeout + new Random().nextInt(random));
-        now = LocalTime.now();
+        this.then = LocalTime.now().plusSeconds(timeout + new Random().nextInt(random));
+        this.now = LocalTime.now();
+    }
+
+    public boolean isOngoing() {
+        this.now = LocalTime.now();
+        return then.isAfter(now);
     }
 
     @Override
     public void run() {
         startTimeout();
-        while (then.isAfter(now)) {
-            now = LocalTime.now();
+        while (isOngoing()) {
+            // Do nothing
         }
+        myProtocol.timeoutEntry(timeoutType);
     }
 }
