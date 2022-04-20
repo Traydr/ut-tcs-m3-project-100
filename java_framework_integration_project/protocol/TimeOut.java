7f95414addda8f687b58870423e06c10a22fc629
s2793199 <a.a.tenhave@student.utwente.nl>
2022-04-20 20:14:54 +0200

    added randomness to time-out

--- java_framework_integration_project/protocol/TimeOut.java
+++ java_framework_integration_project/protocol/TimeOut.java
@@ -3,11 +3,12 @@ package protocol;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Random;
 
 public class TimeOut implements Runnable{
     @Override
     public void run() {
-        LocalTime then = LocalTime.now().plusSeconds(5);
+        LocalTime then = LocalTime.now().plusSeconds(5 + new Random().nextInt(10));
         LocalTime now = LocalTime.now();
         while (then.isAfter(now)) {
             now = LocalTime.now();
